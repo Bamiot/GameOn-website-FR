099f6782d89f35d43fc7429b96699231496cd254
Louis CORD'HOMME <louis.cordhomme@gmail.com>
2022-01-05 20:46:46 +0100

    dry & little fix

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -31,118 +31,63 @@ let mailRegex =
 modalBtn.forEach(btn => btn.addEventListener('click', launchModal))
 closeBtn.addEventListener('click', closeModal)
 submitBtn.addEventListener('click', validForm)
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = 'block'
 }
+
 // close modal form
 function closeModal() {
   modalbg.style.display = 'none'
 }
-// validate form
-function validForm(event) {
-  const firstError = validFirst()
-  const lastError = validLast()
-  const emailError = validEmail()
-  const birthdateError = validBirthDate()
-  const quantityError = validQuantity()
-  const locationError = validLocation()
-  const cguError = validCGU()
-  console.log({
-    firstError,
-    lastError,
-    emailError,
-    birthdateError,
-    quantityError,
-    locationError,
-    cguError
-  })
-  if (
-    firstError ||
-    lastError ||
-    emailError ||
-    birthdateError ||
-    quantityError ||
-    locationError ||
-    cguError
-  ) {
-    event.preventDefault()
-  }
+
+function checkElementError(element, condition) {
+  element.setAttribute('data-error-visible', condition)
+  console.log(element)
+  return condition
 }
 
-// validation functions
-function validFirst() {
-  const element = formData[0]
-  if (firstNameInput.value === '' || firstNameInput.value.length < 2) {
-    element.setAttribute('data-error-visible', true)
-    return true
-  } else {
-    element.setAttribute('data-error-visible', false)
-    return false
-  }
-}
-function validLast() {
-  const element = formData[1]
-  if (lastNameInput.value === '' || lastNameInput.value.length < 2) {
-    element.setAttribute('data-error-visible', true)
-    return true
-  } else {
-    element.setAttribute('data-error-visible', false)
-    return false
-  }
-}
-function validEmail() {
-  const element = formData[2]
-  if (emailInput.value === '' || !emailInput.value.match(mailRegex)) {
-    element.setAttribute('data-error-visible', true)
-    return true
-  } else {
-    element.setAttribute('data-error-visible', false)
-    return false
-  }
-}
-function validBirthDate() {
-  const element = formData[3]
-  if (birthDateInput.value === '') {
-    element.setAttribute('data-error-visible', true)
-    return true
-  } else {
-    element.setAttribute('data-error-visible', false)
-    return false
-  }
-}
-function validQuantity() {
-  const element = formData[4]
-  if (
-    quantityInput.value !== '' &&
-    (quantityInput.value < 1 || quantityInput.value > 99)
-  ) {
-    element.setAttribute('data-error-visible', true)
-    return true
-  } else {
-    element.setAttribute('data-error-visible', false)
-    return false
-  }
-}
-function validLocation() {
-  const element = formData[5]
+// conditions
+const isEmpty = element => element.value === ''
+const textCondidtion = element => isEmpty(element) || element.value.length < 2
+const mailCondidtion = element =>
+  textCondidtion(element) || !mailRegex.test(element.value)
+const quantityCondidtion = element =>
+  !isEmpty(element) && (element.value < 1 || element.value > 99)
+
+// element validation
+const checkFirst = () => checkElementError(formData[0], textCondidtion(firstNameInput))
+const checkLast = () => checkElementError(formData[1], textCondidtion(lastNameInput))
+const checkEmail = () => checkElementError(formData[2], mailCondidtion(emailInput))
+const checkBirthDate = () => checkElementError(formData[3], isEmpty(birthDateInput))
+const checkQuantity = () =>
+  checkElementError(formData[4], quantityCondidtion(quantityInput))
+const checkLocation = () => {
   let flag = false
   for (const location of locations) if (location.checked) flag = true
-  if (quantityInput.value !== '' && !flag) {
-    element.setAttribute('data-error-visible', true)
-    return true
-  } else {
-    element.setAttribute('data-error-visible', false)
-    return false
-  }
+  return checkElementError(locations, !isEmpty(formData[4]) && !flag)
 }
-function validCGU() {
-  const element = formData[6]
-  if (!checkbox1.checked) {
-    element.setAttribute('data-error-visible', true)
-    return true
-  } else {
-    element.setAttribute('data-error-visible', false)
-    return false
+const checkCGU = () => checkElementError(formData[6], !checkbox1.checked)
+// form validation event
+firstNameInput.addEventListener('focusout', checkFirst)
+lastNameInput.addEventListener('focusout', checkLast)
+emailInput.addEventListener('focusout', checkEmail)
+birthDateInput.addEventListener('focusout', checkBirthDate)
+quantityInput.addEventListener('focusout', checkQuantity)
+checkbox1.addEventListener('change', checkCGU)
+
+// validate form
+function validForm(event) {
+  if (
+    checkFirst() ||
+    checkLast() ||
+    checkEmail() ||
+    checkBirthDate() ||
+    checkQuantity() ||
+    checkLocation() ||
+    checkCGU()
+  ) {
+    event.preventDefault()
   }
 }
